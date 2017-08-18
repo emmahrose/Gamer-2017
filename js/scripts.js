0da3dfbc3ae70ba9e075652a0987b5655a70e247
Emmah Rose <emmahrose98@gmail.com>
2017-08-18 17:52:18 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 jQuery(document).ready(function($) {
-  $(".form-model").submit(function(event) {
+  $("form#max").submit(function(event) {
     event.preventDefault()
-    var numb = parseInt($("input#model").val());
+    var numb = parseInt($("input#number").val());
     console.log(numb);
+    countr()
   });
 });
+
+var countr = function(numb) {
+  for (var i = 1; i <= 16; i++) {
+    if (i % 3 === 0) {
+      alert("ping");
+    } else if (i % 5 === 0) {
+      alert("pong");
+    } else {
+      alert(i);
+    }
+  }
+};
