06daa3374a317ee3bb63fc78cfc6ce43c9002b23
Emmah Rose <emmahrose98@gmail.com>
2017-08-20 20:21:54 +0300

    add correct css ,html

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
+//user interface -front end
 jQuery(document).ready(function($) {
   $("form#max").submit(function(event) {
+    $("output").empty();
     event.preventDefault()
     var numb = parseInt($("input#number").val());
     console.log(numb);
     countr()
   });
 });
+//backend
 
 var countr = function(numb) {
   for (var i = 1; i <= 16; i++) {
     if ((i % 3 === 0) && (i % 5 === 0)) {
+      $("#outputList").append('<li>pingpong</li>');
 
-      alert("ping pong");
+
 
     } else if (i % 5 === 0) {
-      alert("pong");
+      $("#outputList").append('<li>pong</li>');
     } else if (i % 3 === 0) {
-      alert("ping");
+      $("#outputList").append('<li>ping</li>');
     } else {
-      alert(i);
+      $("#outputList").append('<li>' + i + "</li>");
     }
+
   }
 };
